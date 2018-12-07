962fed2ae580afa32b0bf92460d14cc9a41a3a5b
Matthew Chan <_mattc@hotmail.com>
2018-12-08 03:32:46 +0800

    Implement Main Menu

---
+++ MHVR Gamecraft2018/Assets/Scripts/ButtonController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonController : MonoBehaviour {
+
+	public void LoadScene(int sceneNumber) {
+		LoadScene(sceneNumber);
+	}
+
+	public void showObject(GameObject gameObject) {
+		gameObject.SetActive(true);
+	}
+
+	public void hideObject(GameObject gameObject) {
+		gameObject.SetActive(false);
+	}
+
+	public void QuitGame() {
+		Application.Quit();
+	}
+}
