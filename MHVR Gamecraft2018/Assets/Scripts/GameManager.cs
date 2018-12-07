5b92110246019168952433f8c2453e82b3fa4b99
Matthew Chan <_mattc@hotmail.com>
2018-12-07 20:41:07 +0800

    Create 2 Player Controls

---
+++ MHVR Gamecraft2018/Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	private const int NUM_PLAYERS = 2;
+	Player[] players;
+
+	void Start() {
+		InitialiseGame();
+	}
+
+	void Update() {
+		if (Input.GetKeyDown(players[0].Left)) {
+			print("hello");
+		}
+	}
+
+	private void InitialiseGame() {
+		InitialisePlayers();
+	}
+
+	private void InitialisePlayers() {
+		players = new Player[NUM_PLAYERS];
+		for (int i = 0; i < NUM_PLAYERS; i++) {
+			print("Create Player " + i);
+			players[i] = Player.Initialise(i);
+		}
+	}
+}
